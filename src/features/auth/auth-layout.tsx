916d5cb575df875a4d5ef1771822d1d3de1376b9
Laouali <laoualibachir2000@gmail.com>
2025-09-25 03:17:34 +0100

    refactor: name changing to LRCN

--- src/features/auth/auth-layout.tsx
+++ src/features/auth/auth-layout.tsx
@@ -10,7 +10,8 @@ export function AuthLayout({ children }: AuthLayoutProps) {
       <div className='mx-auto flex w-full flex-col justify-center space-y-2 py-8 sm:w-[480px] sm:p-8'>
         <div className='mb-4 flex items-center justify-center'>
           <Logo className='me-2' />
-          <h1 className='text-xl font-medium'>Shadcn Admin</h1>
+          {/* <img src='../../assets/images/LOGO.png' alt='Logo' className='me-2' /> */}
+          <h1 className='text-xl font-medium'>LRCN</h1>
         </div>
         {children}
       </div>
