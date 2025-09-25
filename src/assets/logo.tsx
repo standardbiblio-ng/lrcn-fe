eac166a64a9d25ca8b2c44725dd0c28bbbac8a4c
Laouali <laoualibachir2000@gmail.com>
2025-09-25 03:05:59 +0100

    feat(logo);; replace the svg with image

--- src/assets/logo.tsx
+++ src/assets/logo.tsx
@@ -3,22 +3,12 @@ import { cn } from '@/lib/utils'
 
 export function Logo({ className, ...props }: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      id='shadcn-admin-logo'
-      viewBox='0 0 24 24'
-      xmlns='http://www.w3.org/2000/svg'
+    <img
+      src={'./images/LOGO.png'}
+      alt='Logo'
       height='24'
-      width='24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
       className={cn('size-6', className)}
-      {...props}
-    >
-      <title>Shadcn-Admin</title>
-      <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-    </svg>
+      width='24'
+    />
   )
 }
