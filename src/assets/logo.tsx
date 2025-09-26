81156d03677ab48a61ff10220f260002be584893
Laouali <laoualibachir2000@gmail.com>
2025-09-26 13:27:56 +0100

    refactor: fix the image import and display

--- src/assets/logo.tsx
+++ src/assets/logo.tsx
@@ -1,14 +1,15 @@
 import { type SVGProps } from 'react'
+import logoImage from '@/assets/images/LOGO.png'
 import { cn } from '@/lib/utils'
 
 export function Logo({ className, ...props }: SVGProps<SVGSVGElement>) {
   return (
     <img
-      src={'./images/LOGO.png'}
+      src={logoImage}
       alt='Logo'
-      height='24'
-      className={cn('size-6', className)}
-      width='24'
+      height='80'
+      className={cn(className)}
+      width='80'
     />
   )
 }
