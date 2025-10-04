05b68a3ffd220a5d75913d201732770b17447605
Laouali <laoualibachir2000@gmail.com>
2025-10-04 22:18:26 +0100

    feat: update submit application form with success message and improved layout

--- src/features/application/forms/submit-application.tsx
+++ src/features/application/forms/submit-application.tsx
@@ -1,30 +1,20 @@
 function SubmitApplication() {
   return (
     <div className='space-y-4'>
-      <h2 className='text-xl font-semibold'>Academic History</h2>
-      <div>
-        <label className='block text-sm'>Institution</label>
-        <input type='text' className='w-full rounded border px-2 py-1' />
+      <h2 className='font-montserrat mb-1 text-xl font-semibold'>
+        Application Submitted
+      </h2>
+      <div className='mt-8'>
+        <p className='font-montserrat text-md text-active font-normal'>
+          Please fill out all fields.
+        </p>
+        <p className='text-active text-sm font-semibold'>
+          Your application has been submitted successfully
+        </p>
+        <p className='text-active text-sm font-semibold'>
+          Please check your email in the next 24 hours while you await Approvala
+        </p>
       </div>
-      <div>
-        <label className='block text-sm'>Qualification Obtained</label>
-        <select className='w-full rounded border px-2 py-1'>
-          <option>BSc</option>
-          <option>MSc</option>
-          <option>PhD</option>
-        </select>
-      </div>
-      <div className='flex space-x-4'>
-        <div>
-          <label className='block text-sm'>Start Date</label>
-          <input type='date' className='rounded border px-2 py-1' />
-        </div>
-        <div>
-          <label className='block text-sm'>End Date</label>
-          <input type='date' className='rounded border px-2 py-1' />
-        </div>
-      </div>
-      <button className='mt-2 text-blue-600'>+ Click to Add Institution</button>
     </div>
   )
 }
