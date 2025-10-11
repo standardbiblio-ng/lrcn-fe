122bdbe3bea0b1d7287a0416765951545459057e
Laouali <laoualibachir2000@gmail.com>
2025-10-11 01:01:43 +0100

    feat: refactor Settings component by removing sidebar navigation and integrating AccountForm directly

--- src/features/settings/index.tsx
+++ src/features/settings/index.tsx
@@ -1,5 +1,3 @@
-import { Monitor, Bell, Palette, Wrench, UserCog } from 'lucide-react'
-import { Outlet } from 'react-router-dom'
 import { Separator } from '@/components/ui/separator'
 import { ConfigDrawer } from '@/components/config-drawer'
 import { Header } from '@/components/layout/header'
@@ -7,35 +5,7 @@ import { Main } from '@/components/layout/main'
 import { ProfileDropdown } from '@/components/profile-dropdown'
 import { Search } from '@/components/search'
 import { ThemeSwitch } from '@/components/theme-switch'
-import { SidebarNav } from './components/sidebar-nav'
-
-const sidebarNavItems = [
-  {
-    title: 'Profile',
-    href: '/settings',
-    icon: <UserCog size={18} />,
-  },
-  {
-    title: 'Account',
-    href: '/settings/account',
-    icon: <Wrench size={18} />,
-  },
-  {
-    title: 'Appearance',
-    href: '/settings/appearance',
-    icon: <Palette size={18} />,
-  },
-  {
-    title: 'Notifications',
-    href: '/settings/notifications',
-    icon: <Bell size={18} />,
-  },
-  {
-    title: 'Display',
-    href: '/settings/display',
-    icon: <Monitor size={18} />,
-  },
-]
+import { AccountForm } from './account/account-form'
 
 export function Settings() {
   return (
@@ -50,7 +20,7 @@ export function Settings() {
         </div>
       </Header>
 
-      <Main fixed>
+      <Main fixed className='mx-80'>
         <div className='space-y-0.5'>
           <h1 className='text-2xl font-bold tracking-tight md:text-3xl'>
             Settings
@@ -60,14 +30,11 @@ export function Settings() {
           </p>
         </div>
         <Separator className='my-4 lg:my-6' />
-        <div className='flex flex-1 flex-col space-y-2 overflow-hidden md:space-y-2 lg:flex-row lg:space-y-0 lg:space-x-12'>
-          <aside className='top-0 lg:sticky lg:w-1/5'>
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
-          <div className='flex w-full overflow-y-hidden p-1'>
-            <Outlet />
-          </div>
+        {/* <div className='flex flex-1 flex-col space-y-2 overflow-hidden md:space-y-2 lg:flex-row lg:space-y-0 lg:space-x-12'> */}
+        <div className='flex w-full flex-1 overflow-y-hidden p-1'>
+          <AccountForm />
         </div>
+        {/* </div> */}
       </Main>
     </>
   )
