20a01775cf0f856590d5905cf2c217ce129b881e
Laouali <laoualibachir2000@gmail.com>
2025-11-06 17:02:28 +0100

    Update section title in Recommendations component to 'Recommendations Information'

--- src/features/application/forms/recommendations.tsx
+++ src/features/application/forms/recommendations.tsx
@@ -147,7 +147,7 @@ function Recommendations({
         </p>
         <div className='space-y-4'>
           <h2 className='font-montserrat mb-1 text-xl font-semibold'>
-            Document Upload
+            Recommendations Information
           </h2>
           <h4 className='font-montserrat text-md text-active font-normal'>
             Please fill out all fields.
