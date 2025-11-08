115b7c501cf701dde59f3c23153fe506ddcb0542
Laouali <laoualibachir2000@gmail.com>
2025-11-08 01:44:14 +0100

    Add useAuthStore and initialize application data in AuthenticatedLayout

--- src/components/layout/authenticated-layout.tsx
+++ src/components/layout/authenticated-layout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom'
+import { useAuthStore } from '@/stores/auth-store'
 import { getCookie } from '@/lib/cookies'
 import { cn } from '@/lib/utils'
+import { useInitializeApplicationData } from '@/utils/useInitializeApplicationData'
 import { LayoutProvider } from '@/context/layout-provider'
 import { SearchProvider } from '@/context/search-provider'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
@@ -13,6 +15,8 @@ type AuthenticatedLayoutProps = {
 
 export function AuthenticatedLayout({ children }: AuthenticatedLayoutProps) {
   const defaultOpen = getCookie('sidebar_state') !== 'false'
+
+  useInitializeApplicationData()
   return (
     <SearchProvider>
       <LayoutProvider>
